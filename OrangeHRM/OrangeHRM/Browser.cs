fa54b4168f92de83d40ed617c11148f1e2b5641e
Taras <taras.mokretsky@gmail.com>
2019-11-26 16:51:06 +0200

    Added Browser class

---
+++ OrangeHRM/OrangeHRM/Browser.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeHRMTests
+{
+    public class Browser
+    {
+        private static IWebDriver _driver;
+
+        public static IWebDriver Start()
+        {
+            var browser = ConfigurationManager.AppSettings["Browser"];
+
+            switch (browser)
+            {
+                case "Chrome":
+                    _driver = new ChromeDriver();
+                    break;
+
+                case "Firefox":
+                    _driver = new FirefoxDriver();
+                    break;
+
+                default:
+                    _driver = new ChromeDriver();
+                    break;
+            }
+            _driver.Manage().Window.Maximize();
+
+            return _driver;
+        }
+
+        public void OpenUrl()
+        {
+            var str = ConfigurationManager.AppSettings["Url"];
+
+            Driver.Navigate().GoToUrl(str);
+        }
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (_driver == null)
+                {
+                    _driver = Start();
+                }
+                return _driver;
+            }
+        }
+
+        public static void Close()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
---
+++ OrangeHRM/OrangeHRMTests/OpenLoginPage.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Threading;
+
+namespace OrangeHRMTests
+{
+    [TestFixture]
+    public class Test1
+    {
+        Browser browser = new Browser();
+
+        [Test]
+        public void OpenLoginPageTest()
+        {
+            browser.OpenUrl();
+
+            Thread.Sleep(5000);
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            Browser.Close();
+        }
+    }
+}
