03f9f0f3e7a2e96fb04a294553e0ae5e7ef0ca22
Taras <taras.mokretsky@gmail.com>
2020-01-05 18:08:46 +0200

    Added third test

---
+++ OrangeHRM/OrangeHRMTests/OpenLoginPage2.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OrangeHRM.Pages;
+
+namespace OrangeHRMTests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class OpenLoginPage2 : BaseTest
+    {
+        [Test]
+        public void OpenLoginPageTest2()
+        {
+            OpenBrowserAndLogin();
+
+            MainPage.Menu.ExpandMenuTreeAndSeectItem("Admin", "Job", "Job Titles");
+        }
+    }
+}
