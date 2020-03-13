0f55406dd00842965ee07d389252285c361107a1
Taras <taras.mokretsky@gmail.com>
2020-03-13 23:36:45 +0200

    Updated filePath

--- OrangeHRM/OrangeHRMTests/Tests/CreateEmployee.cs
+++ OrangeHRM/OrangeHRMTests/Tests/CreateEmployee.cs
@@ -13,9 +13,9 @@ namespace OrangeHRMTests.Tests
     [Parallelizable]
     public class CreateEmployee : BaseTest
     {
-        private string _path = @"C:\\Users\1\Pictures\Снимок1.PNG";
-        //private static string _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
-        //private string iconPath = Path.Combine(_path, @"OrangeHRMTests\TestFile.jpg");
+        private static string _pathToDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+        private static string _iconPath = Path.Combine(_pathToDirectory, "TestImage.jpg");
+        private string _correctPath = _iconPath.Remove(0, 6);
         private UserDTO _userDTO = new UserDTO();
 
         [Test(Description = "Create new user")]
@@ -28,7 +28,7 @@ namespace OrangeHRMTests.Tests
 
             AppManager.DashboardPage.Menu.ExpandMenuTreeAndSeectItem("PIM", "Add Employee");
 
-            AppManager.AddEmployeePage.AddEmployee(Faker.Name.First(), Faker.Name.Last(), filePath: _path, create: true, userDTO: _userDTO);
+            AppManager.AddEmployeePage.AddEmployee(Faker.Name.First(), Faker.Name.Last(), filePath: _correctPath, create: true, userDTO: _userDTO);
         }
     }
 }
